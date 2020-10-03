9ebe251640a48ce538789355c991f6381e6e866a
sebnar <gomeztovars@gmail.com>
2020-10-03 17:10:39 -0500

    se actualiza crear producto e iniciar sesion

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from "@ionic/angular";
 import { Router } from '@angular/router';
+import { FBservicesService } from "../fbservices.service";
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  //variables para iniciar sesion
+  email
+  password
 
   constructor(
     public navCtrl: NavController,
-    private router: Router
+    private router: Router,
+    private FB: FBservicesService
   ) { }
 
   irPaginaRegistro() {
     this.router.navigate(["register"]);
   }
 
-  ngOnInit() {
+  //Metodo que inicia sesion
+  iniciarSesion() {
+    this.FB.iniciarSesion(this.email, this.password);
   }
 
 }
