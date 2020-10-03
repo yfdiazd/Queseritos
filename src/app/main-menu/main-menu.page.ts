0c2d05de16b31fe1878fb59e4f780b0afca2229d
yfdiazd <fernandadiaz869@gmail.com>
2020-10-03 15:10:50 -0500

    ssubo menú de configuración direccionando a login por el momento

--- src/app/main-menu/main-menu.page.ts
+++ src/app/main-menu/main-menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController, private router:Router) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 
   ngOnInit() {
   }
+
+  irPageTipoQueso(){
+    this.router.navigate(["login"]);
+  }
+
+  irPageEstadoQueso(){
+    this.router.navigate(["login"]);
+  }
+
+  irPageRegistrarProveedor(){
+    this.router.navigate(["login"]);
+  }
+
+  irPageRegistrarProveedor(){
+  this.router.navigate(["login"]);
+  }
+
+  irPageTipoAnticipo(){
+    this.router.navigate(["login"]);
+    }
+  
+  irPageTipoTrueque(){
+      this.router.navigate(["login"]);
+    
+  }
+
+  irPageRegistrarCliente(){
+    this.router.navigate(["login"]);
+  
+}
+
+irPageCrearCiudad(){
+  this.router.navigate(["login"]);
+
+}
+
+irPageRegistrarConductor(){
+  this.router.navigate(["login"]);
+}
 
 }
