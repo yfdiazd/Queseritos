3e5444fc82bf067d3fdb8e263c30f3f86b44f409
aolje <alejoar_p@hotmail.com>
2020-10-04 12:25:48 -0500

    Ajustes del logo del login y se bajan todos los cambios a master, es funcional, se actualiza esta rama para tener un backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./login/register/registrar-usuario.module').then(m => m.RegistrarUsuarioPageModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
