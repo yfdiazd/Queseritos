1fd53bfbccca7a0b1344996b20498eeebf7d4570
aolje <alejoar_p@hotmail.com>
2020-10-05 22:46:11 -0500

    Se prueba consumo de la tabla ciudad exitosamente, queda pendiente implementar en las demas vistas, tanto front como en back

--- src/app/fbservices.service.ts
+++ src/app/fbservices.service.ts
@@ -2,28 +2,17 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import * as firebase from "firebase";
 import { ToastController, AlertController } from "@ionic/angular";
-import { Observable } from 'rxjs';
-import { element } from 'protractor';
 
 
 
 
 @Injectable({
     providedIn: "root"
-
 })
 export class FBservicesService {
-
-
-    //variable que guarda u obtiene el UID del usuario
     usuarioUid: string;
-    //Variables para obtener la fecha actual
-    today: any;
-    dd: any;
-    mm: any;
-    yyyy: any;
-    //Variables para lista de ciudades
-    ciudadesLista: any[];
+    totalGastoP;
+    numeroIngresos;
 
     //Variables para ingresos
     public listI: any[] = [];
@@ -65,20 +54,15 @@ export class FBservicesService {
     ) {
         firebase.initializeApp(this.config);
         this.verificarsesion();
-
     }
-
-
-
     // todos los mentodos que tienen que ver solo con el usuario
     mostrarNombre() {
         firebase
             .database()
             .ref("usuarios/" + this.usuarioUid + "/datosBasicos")
             .on("value", snapshot => {
-                this.usuario = snapshot.val();
+                this.usuario = snapshot.val().usuario;
                 console.log(this.usuario);
-                console.log(this.usuarioUid);
             });
     }
     iniciarSesion(email, password) {
@@ -112,7 +96,7 @@ export class FBservicesService {
 
                     firebase
                         .database()
-                        .ref("usuario/" + this.usuarioUid + "/datosBasicos")
+                        .ref("usuarios/" + this.usuarioUid + "/datosBasicos")
                         .set({
                             usuario: user,
                             email: email
@@ -148,8 +132,7 @@ export class FBservicesService {
                 this.router.navigate(["home"]);
                 this.usuarioUid = firebase.auth().currentUser.uid;
                 this.mostrarNombre();
-                this.getCiudades();
-                console.log("usuario:", this.usuarioUid);
+
             } else {
                 console.log("No hay sesion, toca loguear");
                 this.router.navigate(["login"]);
@@ -197,26 +180,6 @@ export class FBservicesService {
         });
         toast.present();
     }
-    //mensaje que indica la creacion del producto
-    async toastProductoCrado() {
-        const toast = await this.toastController.create({
-            message: "Se ha creado el producto correctamente",
-            color: "danger",
-            duration: 7000
-        });
-        toast.present();
-    }
-    //mensaje que indica la creacion del proveedor
-    async toastProveedorCrado() {
-        const toast = await this.toastController.create({
-            message: "Se ha creado el proveedor de manera correcta",
-            color: "danger",
-            duration: 7000
-        });
-        toast.present();
-
-
-    }
     // Alertas
     async alertRecuperacion() {
         const alert = await this.alertController.create({
@@ -228,178 +191,6 @@ export class FBservicesService {
 
         await alert.present();
     }
-    //toast operacion exitosa
-    async toastOperacionExitosa() {
-        const toast = await this.toastController.create({
-            message: "Operacion ejecutada con exito",
-            color: "danger",
-            duration: 5000
-        });
-        toast.present();
-    }
 
-    //Metodo que permite crear productos
-    crearProdcuto(codigoProducto, descripcionProducto) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/productos/" + codigoProducto)
-            .set({
-                codigo: codigoProducto,
-                descripcion: descripcionProducto
-
-            });
-        this.toastProductoCrado();
-    }
-
-    //Metodo que permite crear proveedores
-    crearProveedor(tipoIdentificacionProveedor, numIndetificacionProveedor, nombreProveedor, apellidoProveedor, telefonoProveedor, direccionProveedor, correoProveedor) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/proveedores/" + numIndetificacionProveedor + "-" + nombreProveedor)
-            .set({
-                tipoIdentificacion: tipoIdentificacionProveedor,
-                numIndetificacion: numIndetificacionProveedor,
-                nombre: nombreProveedor,
-                apellido: apellidoProveedor,
-                telefono: telefonoProveedor,
-                direccion: direccionProveedor,
-                correo: correoProveedor,
-                fechaCreacion: this.fechaActual()
-
-            });
-        this.toastProveedorCrado();
-    }
-
-    //Metodo que permite consultar la fecha actual:
-
-    fechaActual() {
-        this.today = new Date();
-        this.dd = String(this.today.getDate()).padStart(2, '0');
-        this.mm = String(this.today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        this.yyyy = this.today.getFullYear();
-        this.today = this.dd + '/' + this.mm + '/' + this.yyyy;
-        return this.today;
-    }
-
-    //Metodo que permite crear los tipos de identificacion
-    agregarTipoIdentificacion(codigoTipoIdentificacion, descripcionTipoIdentificacion) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/tiposIdentificacion/" + codigoTipoIdentificacion)
-            .set({
-                codigo: codigoTipoIdentificacion,
-                descripcion: descripcionTipoIdentificacion
-            });
-        this.toastOperacionExitosa();
-    }
-
-    //Metodo para agregar estados de producto
-    agregarEstadoProducto(codigoEstadoProducto, descripcionEstadoProducto) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/estadoProductos/" + codigoEstadoProducto)
-            .set({
-                codigo: codigoEstadoProducto,
-                descripcion: descripcionEstadoProducto
-            });
-        this.toastOperacionExitosa();
-    }
-    //Metodo para agregar el tipo de anticipo
-    agregarTipoAnticipo(codigoTipoAnticipo, descripcionTipoanticipo) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/tipoAnticipo/" + codigoTipoAnticipo)
-            .set({
-                codigo: codigoTipoAnticipo,
-                descripcion: descripcionTipoanticipo
-            });
-        this.toastOperacionExitosa();
-    }
-
-    //Metodo para agregar el tipo de trueque.
-    agregarTipoTrueque(codigoTipoTrueque, descripcionTipoTrueque) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/tipoTrueque/" + codigoTipoTrueque)
-            .set({
-                codigo: codigoTipoTrueque,
-                descripcion: descripcionTipoTrueque
-            });
-        this.toastOperacionExitosa();
-    }
-
-    //Metodo que permite crear las ciudades del sistema
-    agregarCiudad(codigoCiudad, describcionCiudad) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/ciudad/" + codigoCiudad)
-            .set({
-                codigo: codigoCiudad,
-                descripcion: describcionCiudad
-            });
-        this.toastOperacionExitosa();
-    }
-    //Metodo que permite agregar clientes
-    agregarCliente(tipoIdentificacion, numeroIdentificacionCliente, nombresClietne, apellidosCliente, empresaCliente, codigoCiudad, celularCliente, direccionCliente, correoCliente) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/cliente/" + numeroIdentificacionCliente + "-" + nombresClietne)
-            .set({
-                identificacion: tipoIdentificacion,
-                numeroIdentificacion: numeroIdentificacionCliente,
-                nombres: nombresClietne,
-                apellidos: apellidosCliente,
-                empresa: empresaCliente,
-                codigoCiudad: codigoCiudad,
-                celular: celularCliente,
-                direccion: direccionCliente,
-                correo: correoCliente
-            });
-        this.toastOperacionExitosa();
-    }
-    //Metodo para agregar conductores
-    agregarConductor(tipoIdentificacionConductor, numeroIdentificacionConductor, nombreConductor, apelidoConductor, celularConductor) {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/cliente/" + numeroIdentificacionConductor + "-" + nombreConductor)
-            .set({
-                identificacion: tipoIdentificacionConductor,
-                numeroIdentificacion: numeroIdentificacionConductor,
-                nombres: nombreConductor,
-                apellidos: apelidoConductor,
-                celular: celularConductor
-            });
-        this.toastOperacionExitosa();
-    }
-    //obtener uid
-    getUID() {
-        this.usuarioUid = firebase.auth().currentUser.uid;
-        return this.usuarioUid;
-    }
-    local;
-    //Obtener lista de ciudades
-    getCiudades() {
-
-        firebase
-            .database()
-            .ref("usuario/" + this.usuarioUid + "/configuracion/" + "/ciudad")
-            .on("value", snapshot => {
-                this.ciudadesLista = [];
-                snapshot.forEach(element => {
-                    this.ciudadesLista.push(element.val());
-                });
-                return this.ciudadesLista;
-            });
-    }
 
 }
-
