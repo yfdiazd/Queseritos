4c4c71509dfb4e6b8051278e430e79999a2eb05f
aolje <alejoar_p@hotmail.com>
2020-10-10 00:30:19 -0500

    Se realiza la vista de edición de una ciudad

--- src/app/home/homeciudades/homeciudades.page.ts
+++ src/app/home/homeciudades/homeciudades.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 import { FBservicesService } from 'src/app/fbservices.service';
+import { __values } from 'tslib';
 @Component({
   selector: 'app-homeciudades',
   templateUrl: './homeciudades.page.html',
@@ -11,15 +12,76 @@ export class HomeciudadesPage implements OnInit {
 
   constructor(private FB: FBservicesService,
     private actionSheetController: ActionSheetController,
+    public alertController: AlertController,
     private router: Router) { }
+
+  codigoCiudad: string;
+  descripcionCiudad: string;
 
   ngOnInit() {
   }
 
-  crear(){
+  crear() {
     this.router.navigate(["crearciudad"])
   }
 
-  
+  confirarEdición(cod,des){
+    this.FB.updateCiudad(cod,des);
+  }
+
+  editar(cod: string, des: string) {
+    this.presentAlertConfirm(cod, des);
+  }
+
+  val1;
+  val2;
+  async presentAlertConfirm(cod, des) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Modificar registro',
+      inputs: [
+        {
+          name: 'Codigo',
+          type: 'text',
+          value: cod,
+          placeholder: ' Codigo'
+        },
+        {
+          name: 'Codigo',
+          type: 'text',
+          value: des,
+          placeholder: ' Descripción',
+          handler: () =>{
+          
+          }
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Modificar',
+          handler: () => {
+            
+            console.log("Se modifico correctamente");
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  // editarCiudad() {
+  //   this.router.navigate(["crearciudad"]);
+  //   // this.FB.updateCiudad(this.codigoCiudad, this.descripcionCiudad);
+  //   //this.FB.deleteCiudad("1602202142339");
+
+  // }
 
 }
