7b8a9f2acea5744892dca91a8e81076d9405a956
sebnar <gomeztovars@gmail.com>
2020-10-12 14:17:43 -0500

    descaga de alejo y se sube

--- src/app/formularios/crearciudad/crearciudad.page.ts
+++ src/app/formularios/crearciudad/crearciudad.page.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { element } from 'protractor';
-
-import { FBservicesService } from '../../fbservices.service';
-=======
 import { Component, Input, OnInit } from "@angular/core";
 import { ModalController } from '@ionic/angular';
 import { FBservicesService } from "../../fbservices.service";
->>>>>>> 2ec8e86889704593082776e53b9022a537d2803c
 
 @Component({
   selector: "app-crearciudad",
@@ -18,57 +11,16 @@ export class CrearciudadPage implements OnInit {
 
   codigoCiudad: string;
   descripcionCiudad: string;
-<<<<<<< HEAD
-  listaBultos: any[] = [];
-  bultoObj: any = null;
-  constructor(
-    private FB: FBservicesService
-  ) { }
-=======
 
   @Input() codigoEdit;
   @Input() descripcionEdit;
   @Input() id;
->>>>>>> 2ec8e86889704593082776e53b9022a537d2803c
 
   constructor(
     private FB: FBservicesService,
     private modalCtrl: ModalController) {
   }
 
-<<<<<<< HEAD
-  agregarBultoLista() {
-
-    this.bultoObj = {
-      bulto: "1",
-      peso: "44"
-    };
-    console.log("Codddddddd" + this.bultoObj.bulto);
-    console.log("pesssssssssssss" + this.bultoObj.peso);
-    this.listaBultos.push(this.bultoObj);
-
-
-  }
-
-  contadorPeso: number;
-  contarPeso() {
-    this.contadorPeso = 0;
-    this.listaBultos.forEach(element => {
-      console.log("Peeepepeeeeee" + element.peso);
-      this.contadorPeso = (this.contadorPeso + parseInt(element.peso));
-    });
-    console.log("Ttotal tttttt " + this.contadorPeso);
-  }
-
-  guardarCiudad() {
-    //this.FB.agregarCiudad(this.codigoCiudad, this.descripcionCiudad);
-    this.contarPeso();
-    console.log("Arrayyyyyy lennnnn " + this.listaBultos.length);
-    console.log("Peso que enviamos es de " + this.contadorPeso)
-    this.FB.agregarPesaje("proveedor", "producto", this.listaBultos.length, this.contadorPeso, this.listaBultos);
-    this.listaBultos = [];
-
-=======
   ngOnInit() { }
 
   guardarCiudad() {
@@ -92,6 +44,5 @@ export class CrearciudadPage implements OnInit {
   }
   volver() {
     this.modalCtrl.dismiss();
->>>>>>> 2ec8e86889704593082776e53b9022a537d2803c
   }
 }
