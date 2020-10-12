91d7cec0b93169ded757ab93cee157b345d5ed68
sebnar <gomeztovars@gmail.com>
2020-10-11 20:14:38 -0500

    se actualiza para descargar

--- src/app/formularios/crearciudad/crearciudad.page.ts
+++ src/app/formularios/crearciudad/crearciudad.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 import { FBservicesService } from '../../fbservices.service';
 
@@ -10,21 +11,45 @@ import { FBservicesService } from '../../fbservices.service';
 export class CrearciudadPage implements OnInit {
   codigoCiudad: string;
   descripcionCiudad: string;
-
+  listaBultos: any[] = [];
+  bultoObj: any = null;
   constructor(
     private FB: FBservicesService
-
   ) { }
 
   ngOnInit() {
   }
 
+  agregarBultoLista() {
+
+    this.bultoObj = {
+      bulto: "1",
+      peso: "44"
+    };
+    console.log("Codddddddd" + this.bultoObj.bulto);
+    console.log("pesssssssssssss" + this.bultoObj.peso);
+    this.listaBultos.push(this.bultoObj);
+
+
+  }
+
+  contadorPeso: number;
+  contarPeso() {
+    this.contadorPeso = 0;
+    this.listaBultos.forEach(element => {
+      console.log("Peeepepeeeeee" + element.peso);
+      this.contadorPeso = (this.contadorPeso + parseInt(element.peso));
+    });
+    console.log("Ttotal tttttt " + this.contadorPeso);
+  }
+
   guardarCiudad() {
     //this.FB.agregarCiudad(this.codigoCiudad, this.descripcionCiudad);
-
-    //this.FB.generarLote();
-    this.FB.agregarPesajeCompra("proveedor", "producto", "1", "30");
-    //this.FB.deleteCiudad("1602202142339");
+    this.contarPeso();
+    console.log("Arrayyyyyy lennnnn " + this.listaBultos.length);
+    console.log("Peso que enviamos es de " + this.contadorPeso)
+    this.FB.agregarPesaje("proveedor", "producto", this.listaBultos.length, this.contadorPeso, this.listaBultos);
+    this.listaBultos = [];
 
   }
 
