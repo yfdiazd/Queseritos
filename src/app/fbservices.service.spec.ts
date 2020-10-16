2c4bfc58724422ecfc12f339331781af449909b7
sebnar <gomeztovars@gmail.com>
2020-10-15 23:45:37 -0500

    ajuste al specs del fbService

--- src/app/fbservices.service.spec.ts
+++ src/app/fbservices.service.spec.ts
@@ -1,12 +1,6 @@
 import { from } from 'rxjs';
 import { TestBed } from '@angular/core/testing';
 import { FBservicesService } from './fbServices.service';
-import { Camera } from '@ionic-native/camera'
-
-
-providers:[
-   Camera
-]
 
 describe('FBservicesService', () => {
      beforeEach(()=> TestBed.configureTestingModule({}));
