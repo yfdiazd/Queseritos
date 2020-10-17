96d369965aa024b4c6d7e3635b38104f108382da
yfdiazd <fernandadiaz869@gmail.com>
2020-10-17 16:59:42 -0500

    a

--- src/app/cards/cardlotes/cardlotes.page.ts
+++ src/app/cards/cardlotes/cardlotes.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { FBservicesService } from 'src/app/fbservices.service';
+import { AlertController } from '@ionic/angular';
+import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
 @Component({
   selector: 'app-cardlotes',
   templateUrl: './cardlotes.page.html',
   styleUrls: ['./cardlotes.page.scss'],
 })
-export class CardlotesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CardlotesPage implements OnInit{
+  idproveedor="Fernanda";
+  numlote: "15-10-2020 - L1";
+  totalBulto: "10";
+  costoTotalCompra: "$1000.000";
+  pesoBultos: "240"
+  constructor(
+    private router: Router,
+    private FB: FBservicesService,
+    private alertController: AlertController,
+    private navCtrl: NavController
+  ) { 
+   // this.FB.listaOrdenLotes()
+    
   }
+  ngOnInit(){
+    
+  }
+
+
+irDetalleLote(){
+  this.navCtrl.navigateForward(["detallelote"]);
+}
+
 
 }
---
+++ src/app/formularios/detallelote/detallelote.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { FBservicesService } from 'src/app/fbservices.service';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-detallelote',
+  templateUrl: './detallelote.page.html',
+  styleUrls: ['./detallelote.page.scss'],
+})
+export class DetallelotePage implements OnInit {
+  Lotenum="17-10-2020-L1"
+  constructor(
+    private router: Router,
+    private FB: FBservicesService,
+    private alertController: AlertController,
+    private navCtrl: NavController
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
