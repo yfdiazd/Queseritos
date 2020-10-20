fb876f6c7462683a149b89f6edb77d9708bee6d5
yfdiazd <fernandadiaz869@gmail.com>
2020-10-19 21:34:38 -0500

    ajuste crear anticipo

--- src/app/formularios/creartrueque/creartrueque.page.ts
+++ src/app/formularios/creartrueque/creartrueque.page.ts
@@ -20,4 +20,43 @@ export class CreartruequePage implements OnInit {
   ngOnInit() {
   }
 
+//   guardar() {
+//     if (this.id == undefined) {
+//       console.log("Entro a crear anticipo")
+//       if (this.tipoAnticipoEdit == undefined || this.valorEdit == undefined || ) {
+//         this.toastCamposRequeridos();
+//       } else {
+//         this.FB.agregarCliente(this.tipoAnticipoEdit, this.valorEdit, "image");
+//         this.modalCtrl.dismiss();
+//       }
+
+//     } else {
+//       console.log("Entro a MODIFICAR---")
+//       if (this.tipoIdentificacionEdit == "" || this.numeroIdentificacionClienteEdit == "" || this.nombresClienteEdit == "" || this.codigociudadEdit == "" || this.celularClienteEdit == "" || this.direccionClienteEdit == "" || this.empresaClienteEdit == "") {
+//         this.toastCamposRequeridos();
+//         console.log("No modificaste nada")
+//       } else {
+//         this.FB.updateCliente(this.id, this.tipoIdentificacionEdit, this.numeroIdentificacionClienteEdit, this.nombresClienteEdit, this.apellidosClienteEdit, this.empresaClienteEdit, this.codigociudadEdit, this.celularClienteEdit, this.direccionClienteEdit, this.correoClienteEdit);
+//         this.modalCtrl.dismiss();
+//       }
+//     }
+//   }
+
+//   volver() {
+//     this.modalCtrl.dismiss();
+//   }
+
+//   async toastCamposRequeridos() {
+//     const toast = await this.toastController.create({
+//       message: "Falta diligenciar campos requeridos.",
+//       cssClass: "toast",
+//       color: 'warning',
+//       position: 'middle',
+//       duration: 5000
+//     });
+//     toast.present();
+//   }
+// }
+
+// 
 }
